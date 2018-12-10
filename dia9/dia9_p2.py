03829a3cc7a657da7a97606eb3c09dcc9437bea5
Abelhg <abelherrero98@gmail.com>
2018-12-10 16:24:52 +0100

    Add files via upload

---
+++ dia9/dia9_p2.py
@@ -0,0 +1,31 @@
+# Advent Of Code: Dia 9 - Segunda parte
+from collections import deque
+
+# Lee entrada
+with open('entradaDia9') as f:
+	inp = f.readlines()
+
+inp = inp[0].split('\n')[0].split(' ')
+numJugadores = int(inp[0])
+ultValor = int(inp[6]) * 100
+puntos = numJugadores * [0]
+tablero = deque([0])
+jugActual = 1
+indActual = 0
+for valor in range(1, ultValor+1):
+	if valor % 23 == 0:
+		# Caso especial multiplo de 23
+		tablero.rotate(7)
+		puntos[jugActual-1] += valor + tablero.pop()
+		tablero.rotate(-1)
+	else:
+		# Caso normal
+		tablero.rotate(-1)
+		tablero.append(valor)
+		
+	jugActual += 1
+	if jugActual > numJugadores:
+		jugActual = 1
+
+# La solucion es la mayor puntuacion
+print "Solucion:", max(puntos)
